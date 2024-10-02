6dedd7c543c64be0f3ded65358e2f1dabbaeef3f
Erik Štvrtecký <estvrtecky@gmail.com>
2024-10-02 10:55:49 +0200

    Add missing page in komentar directory

---
+++ src/app/prispevok/[postId]/komentar/page.tsx
@@ -0,0 +1,13 @@
+// src/app/prispevok/[postId]/komentar/page.tsx
+
+import { Container, Typography } from "@mui/material";
+
+export const metadata = { title: 'List of comments | Insta 2.0' };
+
+export default function CommentList() {
+  return (
+    <Container>
+      <Typography> List of comments </Typography>
+    </Container>
+  );
+}
